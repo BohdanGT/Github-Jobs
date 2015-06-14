88655a48dc5c4ebf736920e2d77cd1272f5d7a64
Bohdan Kachur <bohdan.kachur@live.com>
2015-06-14 16:33:39 +0300

    Saying Hi from book...

--- Github Jobs/AppDelegate.m
+++ Github Jobs/AppDelegate.m
@@ -17,6 +17,7 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
+    NSLog(@"Hi, i'm book created for simple application");
     return YES;
 }
 
